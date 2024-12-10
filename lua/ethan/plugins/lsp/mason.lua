0c99ce20c0aca0b630a8f5fbf6ecdf1adbafa76c
ehancock <ehancock@iastate.edu>
2024-12-10 01:20:44 -0600

    Add formatting, gitsigns, trouble, update mason

--- lua/ethan/plugins/lsp/mason.lua
+++ lua/ethan/plugins/lsp/mason.lua
@@ -1,42 +1,51 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        -- import mason
-        local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-        -- import mason-lspconfig
-        local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-        -- enable mason and configure icons
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
+		local mason_tool_installer = require("mason-tool-installer")
 
-        mason_lspconfig.setup({
-            automatic_installation = true,
-            -- list of servers for mason to install
-            ensure_installed = {
-                "ts_ls",
-                "ast_grep",
-                "html",
-                "cssls",
-                "tailwindcss",
-                "svelte",
-                "lua_ls",
-                "graphql",
-                "emmet_ls",
-                "prismals",
-                "pyright",
-            },
-        })
-    end,
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			automatic_installation = true,
+			-- list of servers for mason to install
+			ensure_installed = {
+				"ts_ls",
+				"ast_grep",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+			},
+		})
+	end,
 }
